a6235fe286d95834fa7e25a827da34c792c49f13
PRATIK DAS <78647354+QuantumQoder@users.noreply.github.com>
2023-11-03 13:25:57 +0530

    Update ErrorDetectionAndCorrection.py
    minor correction

--- ErrorDetectionAndCorrection.py
+++ ErrorDetectionAndCorrection.py
@@ -1,7 +1,7 @@
 # This code uses syndrome measurements and makes only two passes to detect errors and correct them.
 # The code contains a single Bell pair as the code qubits and a single link bit.
-# The errors(identity, x gate, z gate) are introduced in the circuit for each of the two code qubits, thus producing a total of 6 combinations(II, IX, IZ, XI, XX, XZ, ZI, ZX, ZZ)
-# The code considers all the 6 combinations and performs correction in each of the cases, if needed.
+# The errors(identity, x gate, z gate) are introduced in the circuit for each of the two code qubits, thus producing a total of 9 combinations(II, IX, IZ, XI, XX, XZ, ZI, ZX, ZZ)
+# The code considers all the 9 combinations and performs correction in each of the cases, if needed.
 # The code is also scalable, i.e. it can be extended to more number of code bits.
 
 from qiskit import (Aer, ClassicalRegister, QuantumCircuit, QuantumRegister,
